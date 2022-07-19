cd952880c95fb2202bd7e88a209b937b065914ee
SunghaeAn <ahshss903@gmail.com>
2022-07-19 14:48:21 +0900

    코드수정

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         .setColor('#f7ff9c')
         .setTitle('📌GitCat Help')
         .addFields(
-        { name: 'Command', value: '**g.help** -- 이 명령어 모음을 출력합니다.\n**g.github {username}** -- 깃허브를 조회합니다.\n**g.support** -- Gitcat의 서포트서버로 이동합니다.\n**g.report** -- 건의사항을 개발자에게 전송합니다.'}
+        { name: 'Command', value: '**g.help** -- 이 명령어 모음을 출력합니다.\n**g.github {username}** -- 깃허브를 조회합니다.\n**g.support** -- Gitcat의 서포트서버로 이동합니다.\n~~**g.report** -- 건의사항을 개발자에게 전송합니다.~~'}
         )
 
         return message.channel.send({embeds : [gitembed]})
